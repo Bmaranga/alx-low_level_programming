074d44646abb20fe36ce0188058364ea8950dd00
Bmaranga <brianmaranga93@gmail.com>
2022-10-14 14:54:13 -0700

    print various sizes based on type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,13 @@
 
 /**
 * main - function prints the size of various types
+* Return: 0 if doesnt meet expected properties otherwise rint ans
 */
 int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n", sizeof(char));
 	printf("Size of an int: 4 byte(s)\n", sizeof(int));
-	printf("Size of a long int: 8 byte(s)\n", sizeof(long int));
+	printf("Size of a long int: 4 byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
 	printf("Size of a float: 4 byte(s)\n", sizeof(float));
 	return (0);
